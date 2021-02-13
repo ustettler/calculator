cae549295675a1493eaa3bdf6beb783c73faab67
Urs Stettler <boundlessenjoy@gmail.com>
2021-02-13 10:20:33 +0100

    feat: 🎸 js extends

--- rechner.js
+++ rechner.js
@@ -1,9 +1,13 @@
 console.log("Calculator")
+// selekti
 let firstNumber = "";
 let operator = "";
 let secondNumber = "";
 let display = document.querySelector(".display");
 
+// Rechen aufgabe besteht aus. -zahlt operatot zahl-
+// Hier werden 1. und 2. zahl ausgelesen
+// Wenn operator leer ist, ist es erste zahl, wenn es ein operator gibt, zweitezahl
 function addNumber(number){
     console.log(number)
 
@@ -17,33 +21,41 @@ function addNumber(number){
 
 }
 
+// operator geht erst wenn die 1. nummer exisiert
+// wenn 1, nummer existiert, schreibe den operator hinein
 function addOperator(op){
 
     if(firstNumber == ""){
         alert("erstmal eine Nummer")
         return
     } else {
-    operator = op
-    display.value += op
+    operator =  op
+    // Anzeige des Operators mit Abstand auf beiden seiten
+    display.value += " " + op + " "
     }
 }
+
+// rechen funktion ausführen
+// Achtung, geht nur mit VAR!!
 
 function showResult() {
     switch (operator) {
         case '*':
-        let result = Number(firstNumber) * Number(secondNumber)
-        display.value = result
+            var result = Number(firstNumber) * Number(secondNumber )
+            display.value = result
             break;
         case '+':
-           // Hier weiter machen mit Ausrechnung
-
+            var result = Number(firstNumber) + Number(secondNumber)
+            display.value = result
             break;
         case '-':
-
+            var result = Number(firstNumber) - Number(secondNumber)
+            display.value = result
             break;
 
         case '/':
-            
+            var result = Number(firstNumber) / Number(secondNumber)
+            display.value = result
             break;
         
     }
@@ -51,6 +63,14 @@ function showResult() {
     firstNumber = "";
     secondNumber = "";
     operator = "";
+
+
+
 }
 
-//Display leeren vor neuer Operatin
+// Display leeren vor neuer Operation
+
+/* display.addEventListener("click", () => {
+display.value = ""
+}) */
+
