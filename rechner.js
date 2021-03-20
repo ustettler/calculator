c9d75d972aeb9305ee90e5e5d526b1719398b248
Urs Stettler <boundlessenjoy@gmail.com>
2021-03-20 13:31:40 +0100

    fix: 🐛 prettier

--- rechner.js
+++ rechner.js
@@ -1,84 +1,79 @@
-console.log("Calculator")
+console.log("Calculator");
 // selektieren
 let firstNumber = "";
 let operator = "";
 let secondNumber = "";
 let display = document.querySelector(".display");
 let button = document.querySelector(".button");
-let endResult = ""
+let endResult = "";
 
-// Rechen aufgabe besteht aus. -zahlt operatot zahl-
+// Rechen aufgabe besteht aus. -zahlt operator zahl-
 // Hier werden 1. und 2. zahl ausgelesen
 // Wenn operator leer ist, ist es erste zahl, wenn es ein operator gibt, zweitezahl
-function addNumber(number){
-    console.log("nummer: " + number)
+function addNumber(number) {
+	console.log("nummer: " + number);
 
-    if (endResult !== ""){
-        display.value = ""
-        endResult = ""
-    }
+	if (endResult !== "") {
+		display.value = "";
+		endResult = "";
+	}
 
-    if(operator == ""){
-        display.value += number
-        firstNumber += number
-    } else {
-        display.value += number
-        secondNumber += number
-    }
-
+	if (operator == "") {
+		display.value += number;
+		firstNumber += number;
+	} else {
+		display.value += number;
+		secondNumber += number;
+	}
 }
 
 // Operator geht erst wenn die 1. nummer exisiert
 // wenn 1, nummer existiert, schreibe den operator hinein
 
-function addOperator(op){
-
-    if(firstNumber == ""){
-        alert("erstmal eine Nummer")
-        return
-    } else {
-    operator =  op
-    // Anzeige des Operators mit Abstand auf beiden seiten
-    display.value += " " + op + " "
-    }
+function addOperator(op) {
+	if (firstNumber == "") {
+		alert("erstmal eine Nummer");
+		return;
+	} else {
+		operator = op;
+		// Anzeige des Operators mit Abstand auf beiden seiten
+		display.value += " " + op + " ";
+	}
 }
 
 // rechen funktion ausführen
 // Achtung, geht nur mit VAR!!
 
 function showResult() {
-    let result = ""
-    switch (operator) {
-        case '*':
-            result = Number(firstNumber) * Number(secondNumber )
-            display.value = result
-            break;
+	let result = "";
+	switch (operator) {
+		case "*":
+			result = Number(firstNumber) * Number(secondNumber);
+			display.value = result;
+			break;
 
-        case '+':
-            result = Number(firstNumber) + Number(secondNumber)
-            display.value = result
-            break;
+		case "+":
+			result = Number(firstNumber) + Number(secondNumber);
+			display.value = result;
+			break;
 
-        case '-':
-            result = Number(firstNumber) - Number(secondNumber)
-            display.value = result
-            break;
+		case "-":
+			result = Number(firstNumber) - Number(secondNumber);
+			display.value = result;
+			break;
 
-        case '/':
-            result = Number(firstNumber) / Number(secondNumber)
-            display.value = result
-            break;
+		case "/":
+			result = Number(firstNumber) / Number(secondNumber);
+			display.value = result;
+			break;
+	}
 
-    }
+	endResult = result;
 
-    endResult = result
+	console.log("Resultat: " + result);
 
-
-
-        console.log("Resultat: " + result)
-
-    // Alles leeren
-    firstNumber = "";
-    secondNumber = "";
-    operator = "";
+	// Alles leeren
+	firstNumber = "";
+	secondNumber = "";
+	operator = "";
 }
